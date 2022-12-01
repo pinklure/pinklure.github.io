49921a8df6806c5df177e0267c693a08d7254777
pinklure <zhaoqifan0106@qq.com>
2022-12-01 16:00:31 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+DIV("Linux kernel")
+.site("Linux Kernel Documentation", "", "https://docs.kernel.org/index.html")
+.site("Linux Kernel Newbies", "", "https://kernelnewbies.org/")
+.site("Linux Weekly News", "", "https://lwn.net/")
+.site("The Linux Documentation Project", "", "https://tldp.org/")
+.site("Phoronix", "Latest Linux Hardware Reviews, Open-Source News & Benchmarks", "https://www.phoronix.com/")
+.book("The Linux Kernel, by TLDP", "", "https://tldp.org/LDP/tlk/tlk.html")
+.book("Linux Device Drivers, Third Edition", "", "https://lwn.net/Kernel/LDD3/")
+.article("Linux kernel QEMU setup - Víctor Colombo", "", "https://vccolombo.github.io/cybersecurity/linux-kernel-qemu-setup/")
+.article("Booting a Custom Linux Kernel in QEMU and Debugging it with GDB - Nick Desaulniers", "", "http://nickdesaulniers.github.io/blog/2018/10/24/booting-a-custom-linux-kernel-in-qemu-and-debugging-it-with-gdb/")
+.END();
+
+
+
+DIV("QEMU")
+.site("QEMU Document", "", "https://www.qemu.org/docs/master/")
+.article("Setting up QEMU / KQEMU on Ubuntu 7.04 \"Feisty\"", "", "https://ubuntu-tutorials.com/2007/07/04/setting-up-qemu-kqemu-on-ubuntu-704-feisty/")
+.END();
+
+DIV("Miscellany")
+.site("tldr pages", "Simplified and community-driven man pages", "https://tldr.ostera.io/")
+.blog("Corkami -- reverse engineering & technical illustrations", "", "https://corkami.github.io/")
+.END();
---
+++ lib.js
@@ -0,0 +1,57 @@
+class Block {
+    constructor(head) {
+        this.head = head;
+        this.items = [];
+    }
+
+    push(category, title, desc, addr) {
+        this.items.push(
+            {
+                "category": category,
+                "title": title,
+                "desc": desc,
+                "addr": addr,
+            }
+        );
+        return this;
+    }
+
+    site(title, desc, addr) {
+        return this.push("Site", title, desc, addr);
+    }
+
+    blog(title, desc, addr) {
+        return this.push("Blog", title, desc, addr);
+    }
+
+    book(title, desc, addr) {
+        return this.push("Book", title, desc, addr);
+    }
+
+    article(title, desc, addr) {
+        return this.push("Article", title, desc, addr);
+    }
+
+    END() {
+        var header = `<h2>${this.head}</h2>\n`;
+        var body = "";
+        var division = "<hr>\n";
+
+
+        for(var i in this.items) {
+            var entry = 
+`<li> ${this.items[i]["category"]}:
+    <a href="${this.items[i]["addr"]}", target="_blank">
+        ${this.items[i]["title"]}
+    </a> ${this.items[i]["desc"]}
+</li>
+`
+            body += entry;
+        }
+        document.write("<div>" + header + "<ul>" + body + "</ul>" + division + "</div>");   
+    }
+}
+
+function DIV(head){
+    return new Block(head);
+}
