27f8e920bf9b26b69e9f1149aba69457e46c5c63
pinklure <zhaoqifan0106@qq.com>
2022-12-01 17:16:19 +0800

    move browser bookmarks here

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+DIV("eBPF")
+.site("eBPF", "", "https://ebpf.io")
+.site("Cilium", "Linux Native, API-Aware Networking and Security for Containers", "https://cilium.io/")
+.site("Perfetto", "System profiling, app tracing and trace analysis", "https://perfetto.dev/")
+.site("波粒派", "", "https://www.bolipi.com/ebpf/index")
+.site("iovisor/bcc", "", "https://github.com/iovisor/bcc")
+.blog("Brendan Gregg's Homepage", "", "https://www.brendangregg.com/")
+.blog("ARTHURCHIAO'S BLOG", "", "https://arthurchiao.art/index.html")
+.blog("Andrii Nakryiko's Blog", "", "https://nakryiko.com/posts/")
+.article("Linux Performance Analysis in 60,000 Milliseconds", "", "https://netflixtechblog.com/linux-performance-analysis-in-60-000-milliseconds-accc10403c55")
+.article("Learn eBPF Tracing: Tutorial and Examples", "", "https://www.brendangregg.com/blog/2019-01-01/learn-ebpf-tracing.html")
+.END();
+
+
 DIV("Linux kernel")
 .site("Linux Kernel Documentation", "", "https://docs.kernel.org/index.html")
 .site("Linux Kernel Newbies", "", "https://kernelnewbies.org/")
 .site("Linux Weekly News", "", "https://lwn.net/")
 .site("The Linux Documentation Project", "", "https://tldp.org/")
 .site("Phoronix", "Latest Linux Hardware Reviews, Open-Source News & Benchmarks", "https://www.phoronix.com/")
+.site("bootlin", "Read Source Code Online", "https://elixir.bootlin.com/linux/latest/source")
+.site("LWN Kernel index", "articles published in the LWN.net Kernel Page", "https://lwn.net/Kernel/Index/")
+.blog("public0821 的文章 - SegmentFault 思否", "Blog about Linux Networking", "https://segmentfault.com/u/public0821/articles?page=1")
+.blog("Corkami -- reverse engineering & technical illustrations", "", "https://corkami.github.io/")
+.book("Linkers and Loaders", "", "https://www.iecc.com/linkers/")
 .book("The Linux Kernel, by TLDP", "", "https://tldp.org/LDP/tlk/tlk.html")
 .book("Linux Device Drivers, Third Edition", "", "https://lwn.net/Kernel/LDD3/")
+.article("Executable_and_Linkable_Format", "", "https://en.wikipedia.org/wiki/Executable_and_Linkable_Format")
+.article("How to Write Shared Libraries", "", "https://akkadia.org/drepper/dsohowto.pdf")
+.article("The ELF format - how programs look from the inside", "", "https://www.caichinger.com/elf.html")
 .article("Linux kernel QEMU setup - Víctor Colombo", "", "https://vccolombo.github.io/cybersecurity/linux-kernel-qemu-setup/")
 .article("Booting a Custom Linux Kernel in QEMU and Debugging it with GDB - Nick Desaulniers", "", "http://nickdesaulniers.github.io/blog/2018/10/24/booting-a-custom-linux-kernel-in-qemu-and-debugging-it-with-gdb/")
 .END();
-
 
 
 DIV("QEMU")
@@ -17,7 +38,21 @@ DIV("QEMU")
 .article("Setting up QEMU / KQEMU on Ubuntu 7.04 \"Feisty\"", "", "https://ubuntu-tutorials.com/2007/07/04/setting-up-qemu-kqemu-on-ubuntu-704-feisty/")
 .END();
 
+DIV("C++")
+.site("Compiler Explore", "", "https://godbolt.org/")
+.site("C++ Patterns", "", "https://cpppatterns.com/")
+.END();
+
+console.log("What happened?");
+DIV("Caches")
+.article("Gallery of Processor Cache Effects", "", "http://igoro.com/archive/gallery-of-processor-cache-effects/")
+.article("CPU Caches and Why You Care -- Scott Meyers, Ph.D.", "", "https://www.aristeia.com/TalkNotes/ACCU2011_CPUCaches.pdf")
+.END();
+
 DIV("Miscellany")
 .site("tldr pages", "Simplified and community-driven man pages", "https://tldr.ostera.io/")
-.blog("Corkami -- reverse engineering & technical illustrations", "", "https://corkami.github.io/")
+.site("Learning Git Branching", "", "https://learngitbranching.js.org/")
+.site("Command Not Found", "", "https://command-not-found.com/")
+.blog("Some notes on lock-free and wait-free algorithms -- Ross Bencina", "", "http://www.rossbencina.com/code/lockfree?q=~rossb/code/lockfree/")
+.article("Software optimization resources", "", "https://www.agner.org/optimize/")
 .END();
